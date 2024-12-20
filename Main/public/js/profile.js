8121d3a17b9baca857763f700397bf942ed6b19e
Haroutyun Harry Altunyan <haroutyunhaltunyan93@icloud.com>
2024-12-20 12:39:26 -0800

    uncaught typeerror null profile.js:47:3

--- Main/public/js/profile.js
+++ Main/public/js/profile.js
@@ -1,14 +1,14 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const title  = document.querySelector('#blog-title ').value.trim();
-  const needed_funding = document.querySelector('#blog-funding').value.trim();
-  const description = document.querySelector('#blog-desc').value.trim();
+  const title  = document.querySelector('#blog-title').value.trim();
+  
+  const content = document.querySelector('#blog-content').value.trim();
 
-  if (title  && needed_funding && description) {
+  if (title  && content) {
     const response = await fetch(`/api/blogs`, {
       method: 'POST',
-      body: JSON.stringify({ title , needed_funding, description }),
+      body: JSON.stringify({ title, content }),
       headers: {
         'Content-Type': 'application/json',
       },
